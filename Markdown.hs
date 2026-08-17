dafd108333ce98e1ae4c8b953c4e9a5360aeea30
leoweigl <leonhard@weigl.dev>
2026-08-18 00:38:20 +0200

    added block-level code processing

--- Markdown.hs
+++ Markdown.hs
@@ -16,25 +16,50 @@ tagPair (HtmlTag a) = ("<" ++ a ++ ">", "</" ++ a ++ ">")
 main :: IO ()
 main = do
   a <- readFile mdFile
-  let ls = lines a
-  writeFile htmlFile (unlines (renderLines (-1) (markListLines ls)))
+  let rawLines = lines a
+      codeBlockLines = renderCodeBlocks (markCodeBlocks rawLines) False
+      listLines = markListLines codeBlockLines
+  writeFile htmlFile (unlines (renderLines (-1) listLines))
 
 -- Block-level processing (lines -> grouped lines)
 
-markListLines :: [String] -> [(String, Int)]
+markCodeBlocks :: [String] -> [(String, Bool)]
+markCodeBlocks list = case list of
+  [] -> []
+  (x : xs) -> (x, take 3 x == "```") : markCodeBlocks xs
+
+renderCodeBlocks :: [(String, Bool)] -> Bool -> [(String, Bool)]
+renderCodeBlocks list blockActive = case list of
+  [] -> [(closeC ++ closePre, True) | blockActive]
+  (x, y) : xs -> case (blockActive, y) of
+    (False, True) -> (openPre ++ openC, protected) : renderCodeBlocks xs True
+    (True, True) -> (closeC ++ closePre, protected) : renderCodeBlocks xs False
+    (True, _) -> (x, protected) : renderCodeBlocks xs True
+    _ -> (x, protected) : renderCodeBlocks xs False
+    where
+      protected = blockActive || y
+  where
+    (openPre, closePre) = tagPair (HtmlTag "pre")
+    (openC, closeC) = tagPair (HtmlTag "code")
+
+-- bool is for protected items
+markListLines :: [(String, Bool)] -> [(String, Int, Bool)]
 markListLines list = case list of
   [] -> []
-  (x : xs) -> (x, listLevel x) : markListLines xs
+  (x, y) : xs -> (x, listLevel x, y) : markListLines xs
 
-renderLines :: Int -> [(String, Int)] -> [String]
+renderLines :: Int -> [(String, Int, Bool)] -> [String]
 renderLines lastLevel list = case list of
   [] -> [closeLi ++ closeUl | wasLast]
-  (x, currentLevel) : xs -> case (wasLast, isCurrent, compare lastLevel currentLevel) of
-    (_, True, LT) -> openUl : openLi : listItemRest
-    (True, True, EQ) -> closeLi : openLi : listItemRest
-    (True, True, GT) -> closingTags ++ closeLi : openLi : listItemRest
-    (True, _, _) -> closingTags ++ noListItemRest
-    (_, _, _) -> [closeUl | wasLast] ++ noListItemRest
+  (x, currentLevel, protected) : xs ->
+    if protected
+      then x : renderLines currentLevel xs
+      else case (wasLast, isCurrent, compare lastLevel currentLevel) of
+        (_, True, LT) -> openUl : openLi : listItemRest
+        (True, True, EQ) -> closeLi : openLi : listItemRest
+        (True, True, GT) -> closingTags ++ closeLi : openLi : listItemRest
+        (True, _, _) -> closingTags ++ noListItemRest
+        (_, _, _) -> [closeUl | wasLast] ++ noListItemRest
     where
       isCurrent = currentLevel /= -1
       listItemRest = parseListContent x : renderLines currentLevel xs
